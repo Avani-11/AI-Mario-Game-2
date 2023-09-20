cd2127215570377249f7566c7cffef4dc0113da0
Avani-11 <87705294+Avani-11@users.noreply.github.com>
2023-09-20 17:39:50 +0530

    Add files via upload

--- characters_environment.js
+++ characters_environment.js
@@ -53,8 +53,18 @@ var gameConfig={
 =            Game Status             =
 ====================================*/
 
-function game(){
+noseX = "";
+noseY = "";
+GameStatus = "";
 
+function startGame()
+{
+  GameStatus = "start";
+  document.getElementById("status").innerHTML = "Game is Loading";
+}
+
+function game(){
+  console.log("noseX = " + noseX +" , noseY = "+ noseY);
   instializeInDraw();
   moveEnvironment(mario);
   drawSprites();
